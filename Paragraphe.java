44c117db511034df635d3fddcb66850f7b8223b9
4ppolyon <romain.alves@etu.univ-grenoble-alpes.fr>
2023-09-16 22:05:05 +0200

    Paragraphe fini

--- Paragraphe.java
+++ Paragraphe.java
@@ -2,16 +2,37 @@ import java.util.ArrayList;
 
 public class Paragraphe {
     private ArrayList<String> mots;
-    private ArrayList<String> ligne;
     private int largeur;
     private int nbmot;
-    private int nblignes;
+    private int nbligne;
     public Paragraphe(){
         mots = new ArrayList<>();
-        ligne = new ArrayList<>();
         nbmot = 0;
-        nblignes = 0;
+        nbligne = 0;
         largeur = 80;
+    }
+    public void calculeLigne(ArrayList<String> lignes) {
+        int i = 0;
+        nbmot = nbmot();
+        int larg = largeur;
+        while (!(i == nbmot)) {
+            if (larg >= mots.get(i).length() + 1) {
+                lignes.add(mots.get(i) + ' ');
+                larg -= (mots.get(i).length() + 1);
+                i++;
+            } else {
+                if (mots.get(i).length() + 1 > largeur) {
+                    System.out.println("Le mot " + mots.get(i) + " est trop long pour être affiché");
+                    System.exit(1);
+                } else {
+                    larg = largeur;
+                    lignes.add("\n");
+                    nbligne++;
+                }
+            }
+        }
+        lignes.add("\n");
+        nbligne++;
     }
     public void calculeLigne(){
         int i = 0;
@@ -19,16 +40,14 @@ public class Paragraphe {
         int larg = largeur;
         while(!(i==nbmot)){
             if(larg >= mots.get(i).length()+1) {
-                ligne.add(mots.get(i) + ' ');
                 larg -= (mots.get(i).length()+1);
+                i++;
             }else{
                 larg = largeur;
-                ligne.add("\n");
-                nblignes++;
+                nbligne++;
             }
-            i++;
         }
-        ligne.add("\n");
+        nbligne++;
     }
     public void fixeLargeur (int l){
         largeur = l;
@@ -41,14 +60,15 @@ public class Paragraphe {
     }
     public int nblignes(){
         calculeLigne();
-        return nblignes;
+        return nbligne;
     }
 
     @Override
     public String toString() {
-        calculeLigne();
+        ArrayList<String> lignes = new ArrayList<>();
+        calculeLigne(lignes);
         StringBuilder resultat = new StringBuilder();
-        ligne.forEach(resultat::append);
+        lignes.forEach(resultat::append);
         return resultat.toString();
     }
 }
